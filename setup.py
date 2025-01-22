bfd57f97b1b80eaee2705a148799619fd442fc61
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-01-22 07:44:37 +0200

    changed the version number

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='testpackage',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/d1stadeyemi/mypackage',
+    url='https://github.com/d1stadeyemi/testpackage.git',
     author='Muhammad Ajagbe',
     author_email='d1stadeyemi@gmail.com'
 )
